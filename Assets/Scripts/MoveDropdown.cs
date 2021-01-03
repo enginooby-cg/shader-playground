2194f6aa24ae6f335c913383402d1a93305c9f76
Hieu Ngo <justacpulover@gmail.com>
2021-01-03 19:55:34 +0100

    Create new scene for all shaders

---
+++ Assets/Scripts/MoveDropdown.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveDropdown : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Camera flyCamera;
+    public float xOffset = 6f;
+    
+    public void HandleDropdown(int index){
+        flyCamera.transform.position = new Vector3(xOffset * index, 0, 0);
+        flyCamera.transform.eulerAngles = Vector3.zero;
+    }
+}
