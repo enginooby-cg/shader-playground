3b4877892a679dac7e5d3696194c8d748f73adac
Hieu Ngo <justacpulover@gmail.com>
2021-01-03 14:05:21 +0100

    Add singleton template

---
+++ Assets/Scripts/MouseController.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseController : MonoBehaviour
+{
+    [SerializeField] Texture2D arrowCursor;
+    // Start is called before the first frame update
+    void Start()
+    {
+#if UNITY_WEBGL
+        Cursor.SetCursor(arrowCursor, Vector2.zero, CursorMode.ForceSoftware);
+#else
+        Cursor.SetCursor(cursorTexture, hotSpot, CursorMode.Auto);
+#endif
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0) || Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (Cursor.lockState == CursorLockMode.Locked)
+                UnlockMouse();
+        }
+
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            ToggleMouseLock();
+        }
+    }
+
+    private void ToggleMouseLock()
+    {
+        if (Cursor.lockState == CursorLockMode.None)
+        {
+            LockMouse();
+        }
+        else
+        {
+            UnlockMouse();
+        }
+    }
+
+    private void UnlockMouse()
+    {
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+    }
+
+    private void LockMouse()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+}
---
+++ Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using Michsky.UI.ModernUIPack;
+
+public class SceneLoader : MonoBehaviour
+{
+    [SerializeField] SystemInput systemInput;
+    [SerializeField] GameObject mainUI;
+    [SerializeField] CustomDropdown dropdown;
+    [SerializeField] int initialSceneId;
+    SortedDictionary<int, string> scenes; // build index and name
+    void OnEnable()
+    {
+        //Tell our 'OnLevelFinishedLoading' function to start listening for a scene change as soon as this script is enabled.
+        SceneManager.sceneLoaded += OnLevelFinishedLoading;
+    }
+
+    void OnDisable()
+    {
+        //Tell our 'OnLevelFinishedLoading' function to stop listening for a scene change as soon as this script is disabled. Remember to always have an unsubscription for every delegate you subscribe to!
+        SceneManager.sceneLoaded -= OnLevelFinishedLoading;
+    }
+
+    void OnLevelFinishedLoading(Scene scene, LoadSceneMode mode)
+    {
+    }
+
+    private void Start()
+    {
+        Screen.fullScreen = true;
+
+        // init task names
+        scenes = new SortedDictionary<int, string>(){
+            {0, "Dissolve"},
+        };
+
+        if (dropdown == null) return;
+
+        // init dropdown options
+        foreach (string taskName in scenes.Values)
+        {
+            //dropdown.options.Add(new Dropdown.OptionData(taskName));
+            dropdown.CreateNewItemFast(taskName, null);
+        }
+        dropdown.SetupDropdown();
+
+        // load initial dropdown option
+        dropdown.ChangeDropdownInfo(initialSceneId);
+        SceneManager.LoadScene(initialSceneId);
+        mainUI.SetActive(true);
+    }
+
+    public void HandleDropdown(int option)
+    {
+        SceneManager.LoadScene(option);
+        //LockCursor();
+    }
+
+    private void LockCursor()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+
+}
